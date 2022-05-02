a3ccec487069b13d077ecec596e9b82ed9eaed11
Royden Semedo <roydensemedo@Roydens-MacBook-Pro.local>
2022-05-02 14:44:13 -0400

    added instructions and notes to follow

---
+++ __test__/data.test.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const { test } = require("media-typer");
+const { data } = require("../data/db.json");
+
+
+
+test{"creation of a note"};
+test("to find specific notes by id");
+test("to delete a note");
