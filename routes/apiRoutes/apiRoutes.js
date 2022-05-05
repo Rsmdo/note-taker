3734cce8adabd416c507a84f8b22a42631c577d6
Royden Semedo <roydensemedo@Roydens-MacBook-Pro.local>
2022-05-05 12:51:51 -0400

    ADding and saving notes works and unique id with nanoid is created

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -5,12 +5,12 @@ const {createANote} = require('../../lib/data');
 const data = require('../../db/db.json');
 
 router.get('/notes', (req,res) => {
-    res.json(data.slice(1));
+    res.send(data);
 });
 
 router.post('/notes', (req, res) => {
-    const newNote = createANote(req.body, data);
-    res.json(newNote);
+    createANote(req.body);
+    res.json();
 });
 
 
