fdec8c85b95db0a0b9528b453f333717cb36a395
Royden Semedo <roydensemedo@Roydens-MacBook-Pro.local>
2022-05-05 13:03:48 -0400

    added delete note function

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = require("express").Router();
 const fs = require("fs");
-const {createANote} = require('../../lib/data');
+const {createANote, deleteANote} = require('../../lib/data');
 const data = require('../../db/db.json');
 
 router.get('/notes', (req,res) => {
@@ -9,10 +9,14 @@ router.get('/notes', (req,res) => {
 });
 
 router.post('/notes', (req, res) => {
-    createANote(req.body);
+    createANote(req.body); //calling the function created in the data file 
     res.json();
 });
 
+router.delete('/notes/:id', (req, res) => { //the id at the end is to target a specific id
+    deleteANote(req.params);
+    res.json();
+});
 
 
 
